d048f9c4f7b3acd3daa32f4a21bb5020c5d7256e
Sam <villaluzsam@gmail.com>
2017-02-21 02:55:36 -0500

    implement filters, images, controllers

--- src/app.js
+++ src/app.js
@@ -2,6 +2,16 @@
   var app = angular.module('store', [ ]);
 
   //controllers define app behaviour
+  app.controller("PanelController", function(){
+    this.tab = 1;
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
+  });
+
   app.controller('StoreController', function(){
     this.products = gems; //sets property product of controller to gem
   });
@@ -13,12 +23,27 @@
       description: "I dont know how many sides this shape has!",
       canPurchase: true,
       soldOut: true,
+      images: [
+        {
+          full: 'assets/dodecahedron-01-full.jpg',
+          thumb: 'dodecahedron-01-thumb.jpg'
+        },
+        {
+          full: 'assets/dodecahedron-01-full.jpg',
+          thumb: 'dodecahedron-02-thumb.jpg'
+        }
+      ]
     },
     {
       name: "Accelatetragon",
       price: 100.50,
       description: "This product is made up!",
-      canPurchase: false
+      canPurchase: false,
+      images: [
+        {
+          full: 'assets/dodecahedron-02-full.jpg',
+        }
+      ]
     }
   ]
 })();
